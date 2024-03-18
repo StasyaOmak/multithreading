18a501a4740347f049720ca1acb7f5f698cec9ea
Anastasiya Omak <ios.developer.by@gmail.com>
2024-03-18 17:44:41 +0200

    Создан проект многопоточности

---
+++ multithreading/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  multithreading
+//
+//  Created by Anastasiya Omak on 18/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(Thread.current)
+    }
+
+
+}
+
