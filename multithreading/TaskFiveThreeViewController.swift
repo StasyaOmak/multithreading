5c741f38bfa56dde25957858fe21cbaa89edd1ef
Anastasiya Omak <ios.developer.by@gmail.com>
2024-03-25 09:37:56 +0200

    Завершена работа Task 5

---
+++ multithreading/TaskFiveThreeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskFiveThreeViewController.swift
+//  multithreading
+//
+//  Created by Anastasiya Omak on 25/03/2024.
+//
+
+import UIKit
+
+class TaskFiveThreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
